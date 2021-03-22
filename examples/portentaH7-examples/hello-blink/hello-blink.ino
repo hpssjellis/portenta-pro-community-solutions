d83052256d6247b6bc6e1b4d91294c82cac18bd8
Jeremy Ellis <keyfreemusic@gmail.com>
2021-03-22 00:21:56 -0700

    Add files via upload

---
+++ examples/portentaH7-examples/hello-blink/hello-blink.ino
@@ -0,0 +1,39 @@
+/* hello-blink.ino for the Portenta H7
+*  Turns on the blue LED on for one second, then off for three seconds, repeatedly.
+*  also checks if Serial Printing is working
+*  July 22nd, 2020
+*  by Jeremy Ellis
+*  Twitter @rocksetta
+*  Website https://www.rocksetta.com
+*/
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LEDB, OUTPUT);   // LEDB = blue, LEDG or LED_BUILTIN = green, LEDR = red 
+}
+
+void loop() {
+  Serial.println("Serial print works on the M7 core only.");
+  digitalWrite(LEDB, LOW);   // internal LED LOW = on
+  delay(1000);               // wait for a second
+  digitalWrite(LEDB, HIGH);  
+  delay(3000);               
+}
+
+/*
+* Clickable links to helpful information
+* By @rocksetta
+* March, 2021
+*
+*
+*
+*
+* https://store.arduino.cc/usa/portenta-h7
+* https://www.arduino.cc/pro/tutorials/portenta-h7
+* https://github.com/hpssjellis/my-examples-for-the-arduino-portentaH7
+* https://github.com/hpssjellis/portenta-pro-community-solutions
+*/
+
+
+
+
