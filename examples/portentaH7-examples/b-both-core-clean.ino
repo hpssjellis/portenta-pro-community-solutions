c1433a37b8bfe60823192a689a023d854063c7ae
Jeremy Ellis <keyfreemusic@gmail.com>
2021-03-23 06:38:28 -0700

    Create b-both-core-clean.ino

---
+++ examples/portentaH7-examples/b-both-core-clean.ino
@@ -0,0 +1,46 @@
+//  b-both-core-clean.ino
+//  If uploaded to the M7 Core erases what was on it and boots the M4 core
+//  If uploaded to the M4 Core erases what was on it.
+//  By Rocksetta
+
+void setup() {
+  #ifdef CORE_CM7  
+     bootM4();
+  #endif
+}
+
+void loop() {
+ 
+}
+
+
+
+
+
+
+
+
+/*
+* Clickable links for helpful information
+* By @rocksetta
+* March, 2021
+*
+*
+*
+*  Artduino Pro Links:
+*
+*  https://store.arduino.cc/usa/portenta-h7
+*  https://forum.arduino.cc/index.php?board=148.0
+*  https://www.arduino.cc/pro/tutorials/portenta-h7
+*
+*  Rocksetta links:
+* 
+*  https://twitter.com/rocksetta
+*  https://github.com/hpssjellis/portenta-pro-community-solutions
+*  https://github.com/hpssjellis/my-examples-for-the-arduino-portentaH7
+*  https://github.com/hpssjellis/arduino-high-school-robotics-course
+*  https://www.youtube.com/playlist?list=PL57Dnr1H_egtm0pi-okmG0iE_X5dROaLw
+*
+*
+*
+*/
