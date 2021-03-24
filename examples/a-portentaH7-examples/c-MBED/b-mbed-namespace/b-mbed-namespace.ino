5aef1df17dfd019b0ebe91066c22cc1bae56852a
Jeremy Ellis <keyfreemusic@gmail.com>
2021-03-23 23:09:43 -0700

    Create b-mbed-namespace.ino

---
+++ examples/a-portentaH7-examples/c-MBED/b-mbed-namespace/b-mbed-namespace.ino
@@ -0,0 +1,21 @@
+#include "mbed.h"
+using namespace mbed;
+
+DigitalOut LD1(PK_5); // red
+DigitalOut LD2(PK_6); // green
+DigitalOut LD3(PK_7); // blue
+
+void setup() {
+}
+
+void loop() {
+  LD1 = 0;   // red on
+  LD2 = 0;   // green on
+  LD3 = 0;   // blue on
+  wait_us(1000000);  // 1 million = 1 second delay
+  
+  LD1 = 1;  // off
+  LD2 = 1;
+  LD3 = 1;
+  wait_us(2000000);
+}
