0171954f0cc75d975b0858526e0681fb527a0ce2
Jeremy Ellis <keyfreemusic@gmail.com>
2021-03-24 14:42:17 -0700

    Update b-mbed-namespace.ino

--- examples/a-portentaH7-examples/c-MBED/b-mbed-namespace/b-mbed-namespace.ino
+++ examples/a-portentaH7-examples/c-MBED/b-mbed-namespace/b-mbed-namespace.ino
@@ -19,3 +19,38 @@ void loop() {
   LD3 = 1;
   wait_us(2000000);
 }
+
+
+
+
+
+
+/*
+* Clickable links for helpful information
+* By @rocksetta
+* March, 2021
+* GNU GENERAL PUBLIC LICENSE
+* Use at your own risk.
+*
+*
+*
+*  Artduino Pro Links:
+*
+*  https://store.arduino.cc/usa/portenta-h7
+*  https://forum.arduino.cc/index.php?board=148.0
+*  https://www.arduino.cc/pro/tutorials/portenta-h7
+*
+*  Rocksetta links:
+* 
+*  https://twitter.com/rocksetta
+*  https://github.com/hpssjellis/portenta-pro-community-solutions
+*  https://github.com/hpssjellis/my-examples-for-the-arduino-portentaH7
+*  https://github.com/hpssjellis/arduino-high-school-robotics-course
+*  https://www.youtube.com/playlist?list=PL57Dnr1H_egtm0pi-okmG0iE_X5dROaLw
+*
+*
+*
+*/
+
+
+
