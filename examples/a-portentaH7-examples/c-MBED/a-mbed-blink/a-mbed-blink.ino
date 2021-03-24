16fa8e98a6d8dca0e597bbed9def5b4451250fe4
Jeremy Ellis <keyfreemusic@gmail.com>
2021-03-24 14:45:38 -0700

    Update a-mbed-blink.ino

--- examples/a-portentaH7-examples/c-MBED/a-mbed-blink/a-mbed-blink.ino
+++ examples/a-portentaH7-examples/c-MBED/a-mbed-blink/a-mbed-blink.ino
@@ -1,20 +1,20 @@
 #include "mbed.h"
-  mbed::DigitalOut LD1(PK_5); // red
-  mbed::DigitalOut LD2(PK_6); // green
-  mbed::DigitalOut LD3(PK_7); // blue
+  mbed::DigitalOut myLedRed(PK_5); // red
+  mbed::DigitalOut myLedGreen(PK_6); // green
+  mbed::DigitalOut myLedBlue(PK_7); // blue
 
 void setup() {
 }
 
 void loop() {
-  LD1 = 0;   // red on
-  LD2 = 0;   // green on
-  LD3 = 0;   // blue on
+  myLedRed = 0;   // red on
+  myLedGreen = 0;   // green on
+  myLedBlue = 0;   // blue on
   wait_us(1000000);  // 1 million = 1 second delay
   
-  LD1 = 1;  // off
-  LD2 = 1;
-  LD3 = 1;
+  myLedRed = 1;  // off
+  myLedGreen = 1;
+  myLedBlue = 1;
   wait_us(2000000);
 }
 
