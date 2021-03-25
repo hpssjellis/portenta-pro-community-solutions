6dd0e77adccf208828b4a68d5c492c07fe7b8b74
Jeremy Ellis <keyfreemusic@gmail.com>
2021-03-24 20:38:13 -0700

    Add files via upload

---
+++ examples/b-portenta-h7-with-accessories/a-sensors/a-any-variable-resistor-sensor/a-any-variable-resistor-sensor.ino
@@ -0,0 +1,56 @@
+/* 
+*  a-hello-blink.ino for the Portenta H7
+*
+*  Turns on the blue LED for one second, then off for three seconds, repeatedly.
+*  also checks if Serial Printing is working
+*  July 22nd, 2020
+*  by Jeremy Ellis
+*  Twitter @rocksetta
+*  Website https://www.rocksetta.com
+*/
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LEDB, OUTPUT);   // LEDB = blue, LEDG or LED_BUILTIN = green, LEDR = red 
+}
+
+void loop() {
+  Serial.println("Serial print works on the M7 core only.");
+  digitalWrite(LEDB, LOW);   // internal LED LOW = on
+  delay(1000);               // wait for a second
+  digitalWrite(LEDB, HIGH);  
+  delay(3000);               
+}
+
+
+
+
+
+
+/*
+* Clickable links for helpful information
+* By @rocksetta
+* March, 2021
+* GNU GENERAL PUBLIC LICENSE
+* Use at your own risk.
+*
+*
+*
+*  Artduino Pro Links:
+*
+*  https://store.arduino.cc/usa/portenta-h7
+*  https://forum.arduino.cc/index.php?board=148.0
+*  https://www.arduino.cc/pro/tutorials/portenta-h7
+*
+*  Rocksetta links:
+* 
+*  https://twitter.com/rocksetta
+*  https://github.com/hpssjellis/portenta-pro-community-solutions
+*  https://github.com/hpssjellis/my-examples-for-the-arduino-portentaH7
+*  https://github.com/hpssjellis/arduino-high-school-robotics-course
+*  https://www.youtube.com/playlist?list=PL57Dnr1H_egtm0pi-okmG0iE_X5dROaLw
+*
+*
+*
+*/
+
