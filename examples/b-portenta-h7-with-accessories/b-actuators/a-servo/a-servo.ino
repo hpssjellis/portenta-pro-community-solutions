d64f89bc12615824e2a046e85248e4ef4018d248
Jeremy Ellis <keyfreemusic@gmail.com>
2021-03-24 20:44:24 -0700

    Update a-servo.ino

--- examples/b-portenta-h7-with-accessories/b-actuators/a-servo/a-servo.ino
+++ examples/b-portenta-h7-with-accessories/b-actuators/a-servo/a-servo.ino
@@ -1,28 +1,65 @@
-/* 
-*  a-hello-blink.ino for the Portenta H7
-*
-*  Turns on the blue LED for one second, then off for three seconds, repeatedly.
-*  also checks if Serial Printing is working
-*  July 22nd, 2020
-*  by Jeremy Ellis
-*  Twitter @rocksetta
-*  Website https://www.rocksetta.com
-*/
+/*  
+ *   my11-servo.ino
+ *
+ * Note: Needs Servo version above 0.0.2 or you need to make these changes
+ * https://forum.arduino.cc/index.php?topic=691668.msg4700186#msg4700186
+ *
+ *
+ * MUST HAVE A 6 VOLT EXTERNAL BATTERY
+ * Note: REMEMBER 2 WIRES GO TO THE SERVO GROUND (Battery Neg and Board GND)
+ * Servos run from 0 to 180 degrees, test 20 - 160 first
+ * 
+ * servo red to 6V battery positive
+ * servo ground (brown or black) to 6V battery negative
+ * servo ground (brown or black) to board GND
+ * servo controller (orange or the other color) to board PWM pin A4   
+ * 
+ * Note For PWM pins check your boards Pinout diagram.
+ * 
+ *  By Jeremy Ellis twitter @rocksetta
+ *  Webpage http://rocksetta.com
+ *  Arduino High School Robotics Course at
+ *  https://github.com/hpssjellis/arduino-high-school-robotics-course
+ * 
+ *  Update Aug 7th, 2020
+ * 
+ */
+
+#include <Arduino.h>  // only needed for https://platformio.org/
+#include <Servo.h>
+
+Servo myServo4;
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(LEDB, OUTPUT);   // LEDB = blue, LEDG or LED_BUILTIN = green, LEDR = red 
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  myServo4.attach(5);   // D5 should do PWM
+
 }
 
 void loop() {
-  Serial.println("Serial print works on the M7 core only.");
-  digitalWrite(LEDB, LOW);   // internal LED LOW = on
-  delay(1000);               // wait for a second
-  digitalWrite(LEDB, HIGH);  
-  delay(3000);               
+ 
+  digitalWrite(LED_BUILTIN, LOW); // Portenta onboard LED on    
+    
+  myServo4.write(20);           // servo at 20 degrees try 0
+  delay(2000);
+    
+  myServo4.write(160);         // servo at 160 degrees try 180
+  delay(2000);
+
+  myServo4.write(90);          // servo at 90 degrees
+  delay(2000);   
+    
+    
+  for (int myLoop = 20; myLoop <= 160; myLoop++){
+    myServo4.write(myLoop);
+    delay(25);  // short delay 25 ms
+  }
+
+  digitalWrite(LED_BUILTIN, HIGH);     // Portenta onboard LED off
+  delay(5000);            // 5 second delay
+
 }
-
-
 
 
 
