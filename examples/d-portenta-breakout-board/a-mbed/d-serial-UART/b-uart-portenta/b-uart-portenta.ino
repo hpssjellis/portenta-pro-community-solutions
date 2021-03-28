1b60bc464e259a43276958f0dd21109d93665757
Jeremy Ellis <keyfreemusic@gmail.com>
2021-03-28 16:31:28 -0700

    Create b-uart-portenta.ino

---
+++ examples/d-portenta-breakout-board/a-mbed/d-serial-UART/b-uart-portenta/b-uart-portenta.ino
@@ -0,0 +1,50 @@
+/*
+ * Code on the Portenta looks something like
+ *  Note these Portenta defines
+ * 
+ * #define PIN_SERIAL_RX (13ul)
+ * #define PIN_SERIAL_TX (14ul)
+ * 
+ * #define SERIAL2_TX      PA_15
+ * #define SERIAL2_RX      PF_6
+ * 
+ * #define SERIAL2_RTS     PF_8
+ * #define SERIAL2_CTS     PF_9
+ * 
+ * 
+ * #define SERIAL1_TX      (digitalPinToPinName(PIN_SERIAL_TX))
+ * #define SERIAL1_RX      (digitalPinToPinName(PIN_SERIAL_RX))
+ *
+*/
+
+
+#include "mbed.h"
+#include "rtos.h"
+
+//using namespace mbed;  // sometimes needed
+using namespace rtos;
+
+Thread thread;
+
+void myLedBlue_thread(){
+   while (true) {
+      digitalWrite(LEDB, !digitalRead(LEDB));   //switch on / off
+      ThisThread::sleep_for(1000);
+   }
+}
+
+void setup() {
+   pinMode(LEDB, OUTPUT);   // LEDB = blue, LEDG or LED_BUILTIN = green, LEDR = red 
+   thread.start(myLedBlue_thread);
+   Serial.begin(115200);
+   Serial1.begin(115200);
+}
+
+void loop() {
+  if (Serial1.available()) {     // If anything comes in Serial1 (pins 0 & 1)
+    Serial.write(Serial1.read());   // read it and send it out Serial (USB)
+  }
+  
+}
+
+
