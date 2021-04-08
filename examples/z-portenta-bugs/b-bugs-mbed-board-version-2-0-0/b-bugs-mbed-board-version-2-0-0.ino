e7f5ce401c840b883d7318ac751f89043b6ef954
Jeremy Ellis <keyfreemusic@gmail.com>
2021-04-08 11:32:38 -0700

    Update b-bugs-mbed-board-version-2-0-0.ino

--- examples/z-portenta-bugs/b-bugs-mbed-board-version-2-0-0/b-bugs-mbed-board-version-2-0-0.ino
+++ examples/z-portenta-bugs/b-bugs-mbed-board-version-2-0-0/b-bugs-mbed-board-version-2-0-0.ino
@@ -16,6 +16,19 @@
  *
  *
  *
+ * Tested and for me works using the Arduino 1.8.13 IDE:
+ *
+ * Testing March 8th, 2021
+ *
+ * Serial print from M7 core works
+ * On board LED works on both PortentaH7 M7 and M4 cores
+ * Serial.println on M4 core does not crash program (Need RPI to send to M7)
+ *
+ *
+ *
+ *
+ *
+ *
  *
  *
  *
