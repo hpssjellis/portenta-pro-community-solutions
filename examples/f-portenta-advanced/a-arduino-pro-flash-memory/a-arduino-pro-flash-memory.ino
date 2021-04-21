bcf484d205eb1d27aaad5184647b2b8749bdb159
Jeremy Ellis <keyfreemusic@gmail.com>
2021-04-20 23:51:32 -0700

    Create a-arduino-pro-flash-memory.ino

---
+++ examples/f-portenta-advanced/a-arduino-pro-flash-memory/a-arduino-pro-flash-memory.ino
@@ -0,0 +1,20 @@
+/*
+*
+*
+* ******************************************************************
+*
+* Well explained by the Arduino Pro Tutorials:
+*
+* https://www.arduino.cc/pro/tutorials/portenta-h7/por-ard-flash
+*
+* Note: The above can be installed as a proper Arudino library
+* search for "tutorial" in the library manager and install the
+*
+* Arduino_Pro_Tutorials
+*
+*
+* My Portenta Video playlist at:
+* https://www.youtube.com/playlist?list=PL57Dnr1H_egtm0pi-okmG0iE_X5dROaLw
+*
+* *******************************************************************
+*/
