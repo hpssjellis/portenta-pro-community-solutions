c9f976bb4d85bf98f450a2d5658ef165d5c3f35f
Jeremy Ellis <keyfreemusic@gmail.com>
2021-06-17 14:27:50 -0700

    Create b-b-e-stepper-motor.ino

---
+++ examples/b-portenta-h7-with-accessories/b-b-actuators/b-b-e-stepper-motor/b-b-e-stepper-motor.ino
@@ -0,0 +1,23 @@
+// a12-stepper-motor
+// By Jeremy Ellis
+// MIT license
+
+// Connect your stepper driver and run the program
+// Change the direction and see if it works.
+// Then write your own code to control the stepper
+// does delayMicroseconds() make it go faster or slower
+
+void setup() {
+   pinMode(D2, OUTPUT);  // Direction
+   pinMode(D3, OUTPUT);  // Step
+}
+
+void loop() {
+    digitalWrite(D2, 0);  // pick a direction
+    
+    digitalWrite(D3, 1);  // do a step by setting the pulse high
+    delay(3);             // wait 3 ms
+    
+    digitalWrite(D3, 0);  // a pulse needs to come donwn so set it low.
+    delay(3);             // wait 3 ms
+}
