a573960fc58b6cc4af0a56a7a927cb72c99a3f63
Jeremy Ellis <keyfreemusic@gmail.com>
2021-06-18 11:03:03 -0700

    Update a-b-d-blank-dual-core-template.ino

--- examples/a-portentaH7-examples/a-b-utilities/a-b-d-blank-dual-core-template/a-b-d-blank-dual-core-template.ino
+++ examples/a-portentaH7-examples/a-b-utilities/a-b-d-blank-dual-core-template/a-b-d-blank-dual-core-template.ino
@@ -1,11 +1,44 @@
 // absolutely blank, dual core template
 
-
+// Start all M7 core programming
 #ifdef CORE_CM7  
 
-#endif
+// includes
 
+// global variables
+
+
+  void setup() {
+     bootM4();
+     
+  }
+
+  void loop() {
+  
+  }
+  
+
+#endif  // end all M7 core programming
+
+// Start all M4 core Programming
 #ifdef CORE_CM4  
 
-#endif
+
+// includes
+
+// global variables
+
+
+  void setup() {
+
+  }
+
+  void loop() {
+  
+  }
+  
+
+#endif  // end all M4 core programming
+
+
 
