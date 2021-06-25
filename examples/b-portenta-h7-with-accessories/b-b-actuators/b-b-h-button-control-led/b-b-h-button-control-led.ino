455b768627153d684b413c4fd048364163dc5a3a
Jeremy Ellis <keyfreemusic@gmail.com>
2021-06-24 22:38:37 -0700

    Create b-b-h-button-control-led.ino

---
+++ examples/b-portenta-h7-with-accessories/b-b-actuators/b-b-h-button-control-led/b-b-h-button-control-led.ino
@@ -0,0 +1,67 @@
+/* 
+*  .ino for the Portenta H7
+*
+* GNU GENERAL PUBLIC LICENSE
+* Use at your own risk.
+************************************************ Important stuff if needed ****************************************
+*
+* Must connect the short wire of an LED to a small ohm resistor
+* connect the resistor to ground (GND)
+* and the long wire of the LED to a digital output D5
+*
+********************************************************** end ****************************************************
+*
+*  July 22nd, 2020
+*  by Jeremy Ellis
+*  Twitter @rocksetta
+*  Website https://www.rocksetta.com
+*/
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);     // LEDB = blue, LEDG or LED_BUILTIN = green, LEDR = red 
+  pinMode(D5, OUTPUT);              // external LED also connected to GND
+  pinMode(D6, INPUT_PULLDOWN);      // Connect a button or switch from D6to the button to 3V3
+}
+
+void loop() {
+  if (digitalRead(D6)){
+      digitalWrite(LED_BUILTIN, LOW);   // Internal LED on, weird right
+      digitalWrite(D5, HIGH);           // External LED on
+  } else {
+      digitalWrite(LED_BUILTIN, HIGH);   // Internal LED on, weird right
+      digitalWrite(D5, LOW);           // External LED on  
+  }  
+  // no delay needed since button/switch controls the action
+}
+
+
+
+
+
+
+/*
+* Clickable links for helpful information
+* By @rocksetta
+* March, 2021
+* GNU GENERAL PUBLIC LICENSE
+* Use at your own risk.
+*
+*
+*
+*  Artduino Pro Links:
+*
+*  https://store.arduino.cc/usa/portenta-h7
+*  https://forum.arduino.cc/index.php?board=148.0
+*  https://www.arduino.cc/pro/tutorials/portenta-h7
+*
+*  Rocksetta links:
+* 
+*  https://twitter.com/rocksetta
+*  https://github.com/hpssjellis/portenta-pro-community-solutions
+*  https://github.com/hpssjellis/my-examples-for-the-arduino-portentaH7
+*  https://github.com/hpssjellis/arduino-high-school-robotics-course
+*  https://www.youtube.com/playlist?list=PL57Dnr1H_egtm0pi-okmG0iE_X5dROaLw
+*
+*
+*
+*/
