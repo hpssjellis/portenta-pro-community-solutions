6ceb65d9df5884140d7d4f03bd8084699bf0fe6a
Jeremy Ellis <keyfreemusic@gmail.com>
2021-09-01 12:53:55 -0700

    Update b-a-d-RangeFinder.ino

--- examples/b-portenta-h7-with-accessories/b-a-sensors/b-a-d-RangeFinder/b-a-d-RangeFinder.ino
+++ examples/b-portenta-h7-with-accessories/b-a-sensors/b-a-d-RangeFinder/b-a-d-RangeFinder.ino
@@ -9,38 +9,40 @@
 //http://www.robotshop.com/ca/en/hc-sr04-ultrasonic-range-finder.html 
 
 
-unsigned long duration; 
 
 
-void setup() { 
-    Serial.begin(115200);
-    pinMode(LEDB, OUTPUT); // LED on LEDB 
-                           // ultrasonic range finder Robotshop RB-lte-54 
-                           
-                           // GND pin goes to ground 
-    pinMode(D6, INPUT);    // echo 
-    pinMode(D7, OUTPUT);   // Trig 
-                           // VCC pin goes to VIN on the photon 
-} 
+// Note: VCC on RangeFinder must go to +5V on Portenta
+//         GND on rangefinder goes to GND on Portenta 
+         
+int myTriggerPin = D6;  // Trigger on RangeFinder
+int myEchoPin = D7;     // Echo on Rangefinder  
+unsigned long myDuration;
 
-
-void loop(){ 
-        delay(10);                      // even cicuits need a break 
-        digitalWrite(D7, HIGH);         // activate trigger 
-        delayMicroseconds(10); 
-        digitalWrite(D7, LOW);          // de-activate trigger 
-
-        duration = pulseIn(D6, HIGH);   // how long until a reply? 
-                                        // a blocking call so may wait a few seconds                    
-        if (duration > 2000    ){       // raw data from 200 to 16000                                          
-                                        // where  2000 raw = ~35cm,  4000 raw = ~80cm                                    
-            digitalWrite(LEDB, LOW);    // LEDB Blue LED on if far 
-        } else { 
-            digitalWrite(LEDB, HIGH);   // LEDB Blue LED off if close or nothing
-        }  
-    Serial.println("Duration: " + String(duration)  );
+void setup() {
+  Serial.begin(115200);
+  pinMode(myTriggerPin, OUTPUT);
+  pinMode(myEchoPin, INPUT_PULLDOWN);
 }
 
+void loop() {
+  digitalWrite(myTriggerPin, LOW);
+  delayMicroseconds(10); 
+  digitalWrite(myTriggerPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(myTriggerPin, LOW);
+ 
+  myDuration = pulseIn(myEchoPin, HIGH, 40000UL);
+ 
+  Serial.println("Duration us: "+ String(myDuration));
+  delay(50);  // delay only needed for the println 
+ 
+  if (myDuration > 2000    ){       // raw data from 200 to 16000                                          
+                                    // where  2000 raw = ~35cm,  4000 raw = ~80cm                                    
+       digitalWrite(LEDB, LOW);    // LEDB Blue LED on if far 
+    } else { 
+        digitalWrite(LEDB, HIGH);   // LEDB Blue LED off if close or nothing
+    }  
+}
 
 
 
