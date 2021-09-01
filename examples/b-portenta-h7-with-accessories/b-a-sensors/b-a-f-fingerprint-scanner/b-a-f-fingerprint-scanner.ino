cca6431a22a697208a1a859db7aa2159d24a5248
Jeremy Ellis <keyfreemusic@gmail.com>
2021-09-01 14:38:26 -0700

    Update b-a-f-fingerprint-scanner.ino

--- examples/b-portenta-h7-with-accessories/b-a-sensors/b-a-f-fingerprint-scanner/b-a-f-fingerprint-scanner.ino
+++ examples/b-portenta-h7-with-accessories/b-a-sensors/b-a-f-fingerprint-scanner/b-a-f-fingerprint-scanner.ino
@@ -1,43 +1,45 @@
-// testing, might not work
+
+#include <GT5X.h>
+
+/* Count templates */
 
 
+GT5X finger(&Serial1);
+GT5X_DeviceInfo info;
 
-/* 
-	FPS_Enroll.ino - Library example for controlling the GT-511C3 Finger Print Scanner (FPS)
-	Created by Josh Hawley, July 23rd 2013
-	Licensed for non-commercial use, must include this license message
-	basically, Feel free to hack away at it, but just give me credit for my work =)
-	TLDR; Wil Wheaton's Law
+void setup() {
+    Serial.begin(9600);
+    Serial.println("COUNT TEMPLATES test");
 
-	This simple sketch turns the LED on and off similar to the Arduino blink sketch.
-	It is used to show that communications are working.
-	
-	Modified for Particle Photon by Paul Kourany (peekay123), Oct 24, 2105
-*/
+     Serial1.begin(9600);
+     finger.set_led(true);
+     delay(5000);
+     finger.set_led(false);
+     delay(5000);
+     finger.set_led(true);
+     delay(5000);
+     
+    if (finger.begin(&info)) {
+        Serial.println("Found fingerprint sensor!");
+        Serial.print("Firmware Version: "); Serial.println(info.fwversion, HEX);
+    } else {
+        Serial.println("Did not find fingerprint sensor :(");
+        while (1) yield();
+    }
 
-#include "FPS_GT511C3.h"
-
-
-// Hardware setup - FPS connected to:
-//	  digital pin 4(arduino rx, fps tx)
-//	  digital pin 5(arduino tx - 560ohm resistor fps tx - 1000ohm resistor - ground)
-//		this brings the 5v tx line down to about 3.2v so we dont fry our fps
-
-FPS_GT511C3 fps;
-
-void setup()
-{
-	Serial.begin(9600);
-	fps.UseSerialDebug = true; // so you can see the messages in the serial debug screen
-	fps.Open();
+    while (Serial.read() != -1);
+    Serial.println("Send any character to start.\n");
+    while (Serial.available() == 0) yield();
 }
 
-
-void loop()
-{
-	// FPS Blink LED Test
-	fps.SetLED(true); // turn on the LED inside the fps
-	delay(1000);
-	fps.SetLED(false);// turn off the LED inside the fps
-	delay(1000);
+void loop() {
+    uint16_t count;
+    uint16_t rc = finger.get_enrolled_count(&count);
+    if (rc != GT5X_OK) {
+        Serial.println("Could not get count!");
+        return;
+    }
+    
+    Serial.print(count); Serial.println(" templates in database.\r\n");
+    while (1) yield();
 }
