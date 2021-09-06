4cdd86bca67d6adff401203c858aa85646d02696
Jeremy Ellis <keyfreemusic@gmail.com>
2021-09-06 08:14:49 -0700

    Update b-a-h-TFTV2-touchScreen.ino

--- examples/b-portenta-h7-with-accessories/b-a-sensors/b-a-h-TFTV2-touchScreen/b-a-h-TFTV2-touchScreen.ino
+++ examples/b-portenta-h7-with-accessories/b-a-sensors/b-a-h-TFTV2-touchScreen/b-a-h-TFTV2-touchScreen.ino
@@ -1,4 +1,4 @@
-//This example implements a simple sliding On/Off button. The example
+
 // demonstrates drawing and touch operations.
 // needs this library  Adafruit_ILI9341
 // needs this library install as zip file https://github.com/Lennart401/PortentaTouchScreen
@@ -6,21 +6,14 @@
 
 
 #include <Adafruit_GFX.h>
-//#include <SPI.h>
-//#include <Wire.h>
 #include <Adafruit_ILI9341.h>
 #include <PortentaTouchScreen.h>
 
-//Touchscreen X+ X- Y+ Y- pins
-// set For Portenta
-#define XP D5   // can be a digital pin
-#define YP A6  // must be an analog pin, use "An" notation!
-#define XM A7  // must be an analog pin, use "An" notation!
-#define YM D4   // can be a digital pin
 
-//PortentaTouchScreen ts(XP, YP, XM, YM, 300);
-//PortentaTouchScreen ts(A3, A2, A1, A0, 300);  // Yun
-// must be:       digital, analog/digital, analog/digital, digital, resistor across x plate ~300 ohms
+// PortentaTouchScreen ts(XP, YP, XM, YM, 300);  // original
+// PortentaTouchScreen ts(A3, A2, A1, A0, 300);  // Yun
+// Must be:       digital, analog/digital, analog/digital, digital, resistor across x plate ~300 ohms
+// Touchscreen          X+, X-, Y+, Y-, resistor 
 PortentaTouchScreen ts(D5, A6, A7, D4, 300);  // on Portenta
 
 // Set for Portenta
@@ -28,9 +21,10 @@ PortentaTouchScreen ts(D5, A6, A7, D4, 300);  // on Portenta
 #define TFT_DC D6
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
+
 void setup(void)
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   tft.begin();
 
   tft.fillScreen(ILI9341_BLUE);
@@ -44,14 +38,15 @@ void setup(void)
 
 }
 
+
 void loop(){
   
     // a point object holds x y and z(pressure) coordinates
     Point p = ts.getPoint();
 
-    int myX      = map(p.x(), 600, 1800, 0, 240);   // map raw x to real x
-    int myY      = map(p.y(), 600, 1400, 0, 320); // map raw y to real y
-    int myRadius = map(p.z(), 100, 700, 0, 70); // map raw Pressure to real Radius
+    int myX      = map(p.x(), 200, 1400, 0, 240);   // map raw x to real x
+    int myY      = map(p.y(), 600, 1200, 320, 0);   // map raw y to real y
+    int myRadius = map(p.z(), 50, 700, 5, 70);      // map raw Pressure to real Radius
     
     // we have some minimum pressure we consider 'valid'
     // pressure of 0 means no pressing!
@@ -64,7 +59,7 @@ void loop(){
 
         tft.drawCircle(myX, myY, myRadius, random(0xFFFF));    //draw circle, center:(119, 160), color: random
     }
-    delay(100);  // slow it down to read serial monitor can speed it up when working
+    delay(100);  // 100 slow it down to read serial monitor can speed it up when working
 }
 
 
