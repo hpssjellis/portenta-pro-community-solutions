5885fb2486d8d2585b839734718890a882b5d8e2
Jeremy Ellis <keyfreemusic@gmail.com>
2021-09-23 11:57:49 -0700

    Create z-c-gotchas.ino

---
+++ examples/z-portenta-bugs/z-c-gotchas/z-c-gotchas.ino
@@ -0,0 +1,26 @@
+/*  For any gotchas that might not be obvious to a new user
+*
+*  Example: A0, A1, A2, A3 not to be used for general GPIO 
+*  All Analog pins are not to be used for PWM
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*/
+
+
