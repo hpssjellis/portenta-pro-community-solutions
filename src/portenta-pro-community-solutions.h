5cefe105586b85352c2f076c48ccb13cdf0e4e29
Jeremy Ellis <keyfreemusic@gmail.com>
2021-10-11 14:27:33 -0700

    Update portenta-pro-community-solutions.h

--- src/portenta-pro-community-solutions.h
+++ src/portenta-pro-community-solutions.h
@@ -4,6 +4,14 @@
 
 // test this first then make an #ifDef to activate it
 #include <TinyMqttPortentaEthernet.h>
-// nothing here yet
+
+// then test this
+// then figure out how to switch
+//#include <TinyMqttPortentaWifi.h>
+
+
+
+
+
 
 #endif
