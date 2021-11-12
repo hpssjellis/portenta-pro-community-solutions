6fb53b2e64bf838262fbac3cb074ffef20a6a7ba
Jeremy Ellis <keyfreemusic@gmail.com>
2021-11-11 20:20:59 -0800

    Update a-l-simple-timer.ino

--- examples/a-portentaH7-examples/a-l-simple-timer/a-l-simple-timer.ino
+++ examples/a-portentaH7-examples/a-l-simple-timer/a-l-simple-timer.ino
@@ -1,15 +1,19 @@
 
-int myDelay= 2000;   // non-block delay in milliseconds
-unsigned long delayStart = 0;  
+int myDelay = 2000;   // non-block delay in milliseconds
+unsigned long myStart; 
+bool  mySwitch = false; 
 
 void setup() {
-   delayStart = millis();   // set delay
+   myStart = millis();   // set delay
    pinMode(LEDB, OUTPUT);
 }
 
 void loop() {
-    if ( (millis() - delayStart ) >= myDelay) {       
-       delayStart = millis(); //  reset the delay time
+    if ( (millis() - myStart ) >= myDelay) {       
+       myStart = millis();      //  reset the delay time
+       mySwitch = !mySwitch;    // flip the switch ture <--> false
+    }
+    if (mySwitch){
        digitalWrite(LEDB, LOW); // turn the Portenta on-board LED on 
     } else {
        digitalWrite(LEDB, HIGH); // turn the Portenta on-board LED off
