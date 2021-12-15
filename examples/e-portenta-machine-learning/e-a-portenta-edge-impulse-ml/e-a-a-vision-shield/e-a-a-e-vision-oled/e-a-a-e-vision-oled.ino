a78a358df87ea0830eab9ac9e83eb646cc6a4a2f
Jeremy Ellis <keyfreemusic@gmail.com>
2021-12-15 12:29:43 -0800

    Create e-a-a-e-vision-oled.ino

---
+++ examples/e-portenta-machine-learning/e-a-portenta-edge-impulse-ml/e-a-a-vision-shield/e-a-a-e-vision-oled/e-a-a-e-vision-oled.ino
@@ -0,0 +1,251 @@
+/* Edge Impulse Arduino examples
+ * Copyright (c) 2021 EdgeImpulse Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * 
+ *  FOR the GRAYSCALE Waveshare OLED 128 x 128 using library Adafruit_SSD1327.h
+ *   blue  DIN (mosi) D8
+ *   yellow (sck) D9                                                                                                                                                             
+ *   orange (cs) D7
+ *   green (dc)  D6
+ *   white (reset) not needed but D14 if you did
+ *
+ * another reference here 
+ * https://learn.adafruit.com/adafruit-gfx-graphics-library/graphics-primitives
+ *
+ * Note1: Should work with any Edge Impulse model just chnage the below include to your model name
+ * Note2: I simplyify my model order by always using: 0unknown, 1next, 2other, etc, etc
+ */
+
+
+ 
+/* Includes ---------------------------------------------------------------- */
+#include <ei-v20unknown-1popGoRight-2waterGoLeft-3fast-v2-0-0_inferencing.h>
+#include "edge-impulse-advanced.h"
+#include <Adafruit_SSD1327.h>
+
+// Used for software SPI
+#define OLED_CLK D9
+#define OLED_MOSI D8
+
+// Used for software or hardware SPI
+#define OLED_CS D7
+#define OLED_DC D6
+
+// Used for I2C or SPI
+#define OLED_RESET -1
+
+// hardware SPI
+Adafruit_SSD1327 display(128, 128, &SPI, OLED_DC, OLED_RESET, OLED_CS);
+
+// Global Variables
+int myDelay = 0;  // delay between readings, can be zero, default 2000 = 2 seconds
+int x1Map, x2Map, y1Map, y2Map;
+//int myClassCount[EI_CLASSIFIER_LABEL_COUNT]; // not yet used
+
+// the OLED uses these
+#define CUTOUT_COLS                 EI_CLASSIFIER_INPUT_WIDTH
+#define CUTOUT_ROWS                 EI_CLASSIFIER_INPUT_HEIGHT
+const int cutout_row_start = (EI_CAMERA_RAW_FRAME_BUFFER_ROWS - CUTOUT_ROWS) / 2;
+const int cutout_col_start = (EI_CAMERA_RAW_FRAME_BUFFER_COLS - CUTOUT_COLS) / 2;
+
+
+/**
+* @brief      Arduino setup function
+*/
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    Serial.println("Edge Impulse Inferencing Demo");
+
+    pinMode(LEDR, OUTPUT); 
+    pinMode(LEDG, OUTPUT);   // this is LED_BUILTIN
+    pinMode(LEDB, OUTPUT); 
+    
+#ifdef EI_CAMERA_FRAME_BUFFER_SDRAM
+    // initialise the SDRAM
+    SDRAM.begin(SDRAM_START_ADDRESS);
+#endif
+
+    if (ei_camera_init()) {
+        Serial.println("Failed to initialize Camera!");
+    }
+    else {
+        Serial.println("Camera initialized");
+    }
+
+    for (size_t ix = 0; ix < ei_dsp_blocks_size; ix++) {
+        ei_model_dsp_t block = ei_dsp_blocks[ix];
+        if (block.extract_fn == &extract_image_features) {
+            ei_dsp_config_image_t config = *((ei_dsp_config_image_t*)block.config);
+            int16_t channel_count = strcmp(config.channels, "Grayscale") == 0 ? 1 : 3;
+            if (channel_count == 3) {
+                Serial.println("WARN: You've deployed a color model, but the Arduino Portenta H7 only has a monochrome image sensor. Set your DSP block to 'Grayscale' for best performance.");
+                break; // only print this once
+            }
+        }
+    }
+    
+    // Following for the Grayscale OLED
+
+   if ( ! display.begin(0x3D) ) {   // start Grayscale OLED
+     Serial.println("Unable to initialize OLED");
+     while (1) yield();
+  }    
+    display.setTextSize(1);
+    display.setTextColor(SSD1327_WHITE);
+
+    display.setRotation(0);
+    display.setCursor(0,0);
+
+    //   map cutout of the 320 x 320   // 240 model to OLED 128 x 64 screen
+    x1Map = map((int)cutout_col_start, 0, 320, 0, 127);  
+    x2Map = map((int)CUTOUT_COLS, 0, 320, 0, 127);
+    y1Map = map((int)cutout_row_start, 0, 320, 0, 127);
+    y2Map = map((int)CUTOUT_ROWS, 0, 320, 0, 127);
+
+    
+}
+
+/**
+* @brief      Get data and run inferencing
+*
+* @param[in]  debug  Get debug info if true
+*/
+void loop(){
+  
+    // instead of wait_ms, we'll wait on the signal, this allows threads to cancel us...
+    if (ei_sleep(myDelay) != EI_IMPULSE_OK) {
+        return;
+    }
+    
+    Serial.println("Starting inferencing in "+String(myDelay)+" microseconds...");
+
+    // Put the image on the OLED
+    display.clearDisplay();                 // clear the internal memory for OLED
+    for (int x=0; x < EI_CAMERA_RAW_FRAME_BUFFER_COLS; x++){     // EI_CAMERA_RAW_FRAME_BUFFER_COLS = 320
+      for (int y=0; y < EI_CAMERA_RAW_FRAME_BUFFER_ROWS; y++){       //EI_CAMERA_RAW_FRAME_BUFFER_ROWS = 320   //240
+      
+        uint8_t myGRAY = ei_camera_frame_buffer[(y * (int)EI_CAMERA_RAW_FRAME_BUFFER_COLS) + x];  
+
+          int myGrayMap = map(myGRAY, 0, 255, 0, 15);  
+          int xMap = map(x, 0, 320, 0, 127);
+          int yMap = map(y, 0, 320, 0, 127);
+          display.drawPixel(xMap, yMap, myGrayMap );   // grayscale 0-255, 128x128  //128 x 64
+      } 
+    }
+
+    display.drawRect(2, 2,   126, 126, SSD1327_WHITE );  // rectangle around outside of OLED
+
+    display.setCursor(20,10);
+    display.println("Rocksetta");
+
+
+
+
+
+    ei::signal_t signal;
+    signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
+    signal.get_data = &ei_camera_cutout_get_data;
+
+    if (ei_camera_capture((size_t)EI_CLASSIFIER_INPUT_WIDTH, (size_t)EI_CLASSIFIER_INPUT_HEIGHT, NULL) == false) {
+        Serial.println("Failed to capture image\r\n");
+        return;
+    }
+
+    // Run the classifier
+    ei_impulse_result_t result = { 0 };
+
+    EI_IMPULSE_ERROR err = run_classifier(&signal, &result, debug_nn);
+    if (err != EI_IMPULSE_OK) {
+        Serial.println("ERR: Failed to run classifier, error number: " + String(err));
+        return;
+    }
+
+
+    int myBestClassificationNumber = -1;  
+    float myBestClassificationValue = 0.25;   // lowest best allowable value
+    
+    
+    // print the predictions
+    // Serial.println("Predictions ");
+    // For complex prints best to run Edge Impulse ei_printf
+    ei_printf("(DSP: %d ms., Classification: %d ms., Anomaly: %d ms.)",
+        result.timing.dsp, result.timing.classification, result.timing.anomaly);
+    ei_printf(": \n");
+    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+       // ei_printf("    %s: %.5f\n", result.classification[ix].label, result.classification[ix].value);
+
+        if (result.classification[ix].value > myBestClassificationValue ){
+           myBestClassificationNumber = ix;                      // find the biggest array value
+           myBestClassificationValue = result.classification[ix].value;  
+        }
+
+  
+    }
+    
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+    Serial.println("    anomaly score: " + String(result.anomaly, 5));
+#endif
+
+
+    digitalWrite(LEDB, HIGH);   //on board LED's are turned off by HIGH    
+    digitalWrite(LEDG, HIGH);   
+    digitalWrite(LEDR, HIGH); 
+/*
+    // I find it less confusing if the 0unknown does nothing
+    if (myBestClassificationNumber == 0){    // 0 unknown do nothing
+        digitalWrite(LEDB, LOW);    
+        digitalWrite(LEDG, LOW);   
+       // Serial.println("0 unknown: " + String(myBestClassificationValue,2));
+       display.setCursor(5,110);
+       display.println("0 Unknown: " + String(myBestClassificationValue,2));
+    }
+ */
+
+    if (myBestClassificationNumber == 1){   // 1pop: Go Right
+      digitalWrite(LEDB, LOW);              // Blue LED on
+      ei_printf("1Pop Go Right: %.5f\n", myBestClassificationValue);
+      display.setCursor(5,110);
+      display.println("1Pop Go Right:" + String(myBestClassificationValue,2));
+    }
+    
+    if (myBestClassificationNumber == 2){   // 2water : go left
+      digitalWrite(LEDG, LOW);              // Green LED on  
+      ei_printf("2Water Go Left: %.5f\n", myBestClassificationValue);
+      display.setCursor(5,110);
+      display.println("2Water Go Left:" + String(myBestClassificationValue,2));
+    }
+    
+    if (myBestClassificationNumber == 3){   // 3fast got straight
+      digitalWrite(LEDR, LOW);              // Red LED on         
+      ei_printf("3Both Go Fast: %.5f\n", myBestClassificationValue);
+      display.setCursor(5,110);
+      display.println("3Both Go Fast:" + String(myBestClassificationValue,2));
+    }
+
+     // put more if statements here for your model
+
+
+
+     // Last thing is to show the 128x128 GRAYSCALE OLED
+     display.display();  // OLED 4 bit 16 color GRAYSCALE update
+}
