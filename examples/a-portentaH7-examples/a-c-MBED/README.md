e848e49517eec5d36f3eb8d329b8055a9a698135
Jeremy Ellis <keyfreemusic@gmail.com>
2021-12-16 21:02:21 -0800

    updated number scheme

--- examples/a-portentaH7-examples/a-c-MBED/README.md
+++
@@ -1,3 +0,0 @@
-Great resource here at the MBED API site
-
-https://os.mbed.com/docs/mbed-os/v6.9/apis/index.html
---
+++ examples/dot8-portenta-bugs/dot84-bugs-mbed-board-version-2-6-1/dot84-bugs-mbed-board-version-2-6-1.ino
@@ -0,0 +1,18 @@
+/*
+# Version MBED core 2.5.2 Bugs?
+#
+# I will test Camera 320x320, ALso tinyMQTT, and of course the memory splitting
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+*/
