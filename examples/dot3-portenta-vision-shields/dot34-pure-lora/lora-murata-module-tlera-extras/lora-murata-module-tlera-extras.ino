ce59bf3af0a71397932be99dc0729ea10778015a
Jeremy Ellis <keyfreemusic@gmail.com>
2022-01-01 08:17:11 -0800

    Create lora-murata-module-tlera-extras.ino

---
+++ examples/dot3-portenta-vision-shields/dot34-pure-lora/lora-murata-module-tlera-extras/lora-murata-module-tlera-extras.ino
@@ -0,0 +1,21 @@
+/*
+*
+* This stuff is advanced, probably a good idea not to do it
+*
+* Needs a linux computer
+*
+* Explained here, must be the vision shield branch
+*
+* https://github.com/hpssjellis/ArduinoCore-stm32l0/tree/vision_shield
+*
+* 
+*
+*
+*
+*
+*
+*
+*
+*
+*/
+
