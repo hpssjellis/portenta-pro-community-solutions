3e3b95ed700b18ec526421c0d27db01a97ed583d
Jeremy Ellis <keyfreemusic@gmail.com>
2022-02-27 10:50:30 -0800

    Create dot63-NOT-UART-sram-share-core-memory.ino

---
+++ examples/dot1-portentaH7-examples/dot19-cores-uart-serial-com/dot63-NOT-UART-sram-share-core-memory/dot63-NOT-UART-sram-share-core-memory.ino
@@ -0,0 +1,9 @@
+/*
+*   Go to dot63-sram-share-core-memory in the library to find an advanced but very good way
+*   to transfer data between the cores
+*
+*  Direct online link   
+*  https://github.com/hpssjellis/portenta-pro-community-solutions/blob/main/examples/dot6-portenta-advanced/dot63-sram-share-core-memory/dot63-sram-share-core-memory.ino
+*
+*
+*/
