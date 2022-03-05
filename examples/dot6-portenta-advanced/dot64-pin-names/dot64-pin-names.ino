48c2108b9a99b4a8d666dba7e1066fa8213f6984
Jeremy Ellis <keyfreemusic@gmail.com>
2022-03-05 08:57:45 -0800

    Update dot64-pin-names.ino

--- examples/dot6-portenta-advanced/dot64-pin-names/dot64-pin-names.ino
+++ examples/dot6-portenta-advanced/dot64-pin-names/dot64-pin-names.ino
@@ -1,8 +1,34 @@
 #include "pinDefinitions.h"
 
 
+//String myPinNameFunction(int thePin){
+//  return "P" + String(char('A' + (digitalPinToPinName(thePin) / 16))) +"_" + String(digitalPinToPinName(thePin) % 16);
+//}
+
+// thanks Arduino for the following
 String myPinNameFunction(int thePin){
-  return "P" + String(char('A' + (digitalPinToPinName(thePin) / 16))) +"_" + String(digitalPinToPinName(thePin) % 16);
+  int realPin = digitalPinToPinName(thePin) % ALT0;
+  if (realPin == NC) {
+    return "Not a pin";
+  }
+  String extra = "";
+  switch (digitalPinToPinName(thePin) & DUAL_PAD) {
+    case 0:
+      break;
+    case ALT0:
+      extra = "ALT0";
+      break;
+    case ALT1:
+      extra = "ALT1";
+      break;
+    case ALT2:
+      extra = "ALT2";
+      break;
+    case DUAL_PAD:
+      extra = "_C";
+      break;
+  }
+  return "P" + String(char('A' + (realPin / 16))) +"_" + String(realPin % 16) + extra;
 }
 
 void setup() {
@@ -17,7 +43,7 @@ void setup() {
       String myName = myPinNameFunction(myLoop);
       PinName myIn  = digitalPinToPinName(myLoop);
       int myIndex   = PinNameToIndex(myIn);
-      Serial.println("Pin Index " + String(myLoop) + ": " + myName + ", Back to pin index: " + String(myIndex) );
+      Serial.println("Pin Index " + String(myLoop) + ": " + myName + ", Back to pin index: " + String(myIndex)+ "    " );
    }
    Serial.println("-----------------------------");
   
