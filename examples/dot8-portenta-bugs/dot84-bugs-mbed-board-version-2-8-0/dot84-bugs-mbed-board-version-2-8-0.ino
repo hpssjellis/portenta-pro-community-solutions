f6f217037865de725b8985a012ade000a6c54c5d
Jeremy Ellis <keyfreemusic@gmail.com>
2022-03-08 08:18:17 -0800

    Update dot84-bugs-mbed-board-version-2-8-0.ino

--- examples/dot8-portenta-bugs/dot84-bugs-mbed-board-version-2-8-0/dot84-bugs-mbed-board-version-2-8-0.ino
+++ examples/dot8-portenta-bugs/dot84-bugs-mbed-board-version-2-8-0/dot84-bugs-mbed-board-version-2-8-0.ino
@@ -1,9 +1,16 @@
 /*
 *
-* Possibly the cameraClass type has been changed to Camera cam(himax);
+* Possibly the CameraClass type has been changed to Camera example from:
+
+static CameraClass cam;
+
+* to:
+
+Camera cam(himax);
+
 * Can anyone confirm?
 *
-*
+* This will destroy all my camera examples, and not make anything backward compatible for old versions of the MBED core.
 *
 *
 *
