98f71504227cf6d2f5744c65c35f317a815d67a3
Jeremy Ellis <keyfreemusic@gmail.com>
2022-03-27 19:17:25 -0700

    Update dot5114-vision-oled.ino

--- examples/dot5-portenta-machine-learning/dot51-portenta-edge-impulse-ml/dot511-vision-classify/dot5114-vision-oled/dot5114-vision-oled.ino
+++ examples/dot5-portenta-machine-learning/dot51-portenta-edge-impulse-ml/dot511-vision-classify/dot5114-vision-oled/dot5114-vision-oled.ino
@@ -38,7 +38,7 @@
  
 /* Includes ---------------------------------------------------------------- */
 #include <ei-v20unknown-1popGoRight-2waterGoLeft-3fast-v2-0-0_inferencing.h>
-#include "edge-impulse-advanced.h"
+#include "edge-impulse-advanced-v2.h"
 #include <Adafruit_SSD1327.h>
 
 // Used for software SPI
