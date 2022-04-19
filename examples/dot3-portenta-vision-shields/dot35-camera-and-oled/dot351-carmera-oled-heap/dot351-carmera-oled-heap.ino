a24563d6d58c2143d2d8274914b57478d94dd0cd
Jeremy Ellis <keyfreemusic@gmail.com>
2022-04-19 15:06:31 -0700

    Create dot351-carmera-oled-heap.ino

---
+++ examples/dot3-portenta-vision-shields/dot35-camera-and-oled/dot351-carmera-oled-heap/dot351-carmera-oled-heap.ino
@@ -0,0 +1,117 @@
+/*
+ * 
+ * Must use portenta with Vision camerera and wAVESHARE gRAYSCALE 128X128 oled
+ * Should be implemented with MBED version greater than 3.0.0
+ *
+ * Camera memory frame buffer should use the heap
+ *
+ * Purchase here https://www.waveshare.com/1.5inch-OLED-Module.htm about $29 USD
+ *
+ *
+ *  FOR the GRAYSCALE Waveshare OLED
+ *   black GND 
+ *   red 3v3     
+ *   blue  DIN (mosi) D8
+ *   yellow (sck) D9 
+ *   orange (cs) D7
+ *   green (dc)  D6
+ *   white (reset) not needed but D14 if you did
+ *
+ * another reference here 
+ * https://learn.adafruit.com/adafruit-gfx-graphics-library/graphics-primitives
+ *
+ */
+
+ 
+ 
+#include <Arduino.h>  // only needed for https://platformio.org/
+
+#include <Adafruit_SSD1327.h>
+
+// Used for software SPI
+#define OLED_CLK D9  //yellow wire
+#define OLED_MOSI D8 // blue wire 
+
+// Used for software or hardware SPI
+#define OLED_CS D7  // orange wire
+#define OLED_DC D6   // green wire 
+
+// Used for I2C or SPI
+#define OLED_RESET -1
+
+// hardware SPI
+Adafruit_SSD1327 display(128, 128, &SPI, OLED_DC, OLED_RESET, OLED_CS);
+
+
+#include "camera.h"
+#include "himax.h"
+HM01B0 himax;
+Camera cam(himax);
+#define IMAGE_MODE CAMERA_GRAYSCALE
+
+/*
+Other buffer instantiation options:
+  FrameBuffer fb(0x30000000);
+  FrameBuffer fb(320,320,2);
+*/
+
+FrameBuffer fb;
+#define ALIGN_PTR(p,a)   ((p & (a-1)) ?(((uintptr_t)p + a) & ~(uintptr_t)(a-1)) : p)
+
+static uint8_t *ei_camera_frame_mem;
+static uint8_t *frame_buffer; // 32-byte aligned
+
+void setup() {
+  Serial.begin(115200);  
+
+  // Init the cam 
+ // cam.begin(CAMERA_R320x320, IMAGE_MODE, 30);
+  cam.begin(CAMERA_R320x320, IMAGE_MODE, 60);
+
+  if ( ! display.begin(0x3D) ) {
+     Serial.println("Unable to initialize OLED");
+     while (1) yield();
+  }    
+    display.setTextSize(1);
+    display.setTextColor(SSD1327_WHITE);
+
+    display.setRotation(0);
+    display.setCursor(0,0);
+
+   // set framebuffer into heap
+    ei_camera_frame_mem = (uint8_t *) malloc(320 * 320 + 32 /*alignment*/);
+    if(ei_camera_frame_mem == NULL) {
+        Serial.println("failed to create ei_camera_frame_mem"); 
+    }
+    frame_buffer = (uint8_t *)ALIGN_PTR((uintptr_t)ei_camera_frame_mem, 32);
+   
+}
+
+void loop() {
+  display.clearDisplay();                 // clear the internal memory
+    
+  if (cam.grabFrame(fb, 3000) == 0) {
+     // Serial.write(fb.getBuffer(), cam.frameSize());
+    fb.setBuffer(frame_buffer); 
+
+    for (int x=0; x < 320; x++){     // FRAME_BUFFER_COLS = 320
+       for (int y=0; y < 320; y++){       //FRAME_BUFFER_ROWS = 320
+          uint8_t myGRAY = frame_buffer[(y * 320) + x];  
+
+          int myGrayMap = map(myGRAY, 0, 255, 0, 15);  
+          int xMap = map(x, 0, 320, 0, 127);
+          int yMap = map(y, 0, 320, 0, 127);
+          display.drawPixel(xMap, yMap, myGrayMap );   // grayscale 0-255, 128x128  //128 x 64
+      } 
+    }     
+  }
+
+  display.drawRect(0,0,128,128, SSD1327_WHITE ); // border around the screen
+  display.setCursor(3,3);
+  display.println("Rocksetta");
+    
+  display.display();
+       
+}
+
+
