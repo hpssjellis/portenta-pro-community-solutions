364400ed69263df86a1abfe21131c6cf694307ca
Jeremy Ellis <keyfreemusic@gmail.com>
2022-04-20 13:42:53 -0700

    Update dot83-bugs-mbed-board-version-3-0-1.ino

--- examples/dot8-portenta-bugs/dot83-bugs-mbed-board-version-3-0-1/dot83-bugs-mbed-board-version-3-0-1.ino
+++ examples/dot8-portenta-bugs/dot83-bugs-mbed-board-version-3-0-1/dot83-bugs-mbed-board-version-3-0-1.ino
@@ -1,1 +1,40 @@
-fb
+/*
+ *  No Issues yet. April 2oth, 2022
+ *
+ *
+ * Observation Ethernet does not seem to have secure HTTPS ability, whereas WiFi does
+ *
+ * Observation: Can WiFi or Ethernet or BLE be controlled by the M4 core?
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+*/
