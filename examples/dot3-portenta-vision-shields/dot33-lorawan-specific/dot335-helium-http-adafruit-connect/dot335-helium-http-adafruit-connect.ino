4bede737e33a7aed452c37ad40fe41aa1babf38c
Jeremy Ellis <keyfreemusic@gmail.com>
2022-05-04 23:43:54 -0700

    Update dot335-helium-http-adafruit-connect.ino

--- examples/dot3-portenta-vision-shields/dot33-lorawan-specific/dot335-helium-http-adafruit-connect/dot335-helium-http-adafruit-connect.ino
+++ examples/dot3-portenta-vision-shields/dot33-lorawan-specific/dot335-helium-http-adafruit-connect/dot335-helium-http-adafruit-connect.ino
@@ -1,5 +1,28 @@
 /*
   Helium Send And Receive
+
+   For Adafruit non-CayeenLPP connections for simple sensor Bae 64 encoded
+
+  In Helium "functions" section  use this decoder
+
+ function Decoder(bytes, port, uplink_info) {
+  var decoded={};
+  try{
+  var result = String.fromCharCode.apply(null, bytes);
+  decoded.value = result;
+  return decoded;
+  } catch (err) {
+  return 'Decoder: ' + err.name + " : " + err.message;;
+  }
+ }
+
+ And in the integration section of helium use this JSON
+
+ { "value": {{decoded.payload.value}} }
+
+
+
+  
   This sketch demonstrates how to send and receive data with the MKR WAN 1300/1310 LoRa module.
   This example code is in the public domain.
   note: Helium must be setup for what it does with the CayenneLPP encoded data
@@ -7,11 +30,9 @@
 
 #include <Arduino.h> // Only needed by https://platformio.org/
 #include <MKRWAN.h>
-#include <CayenneLPP.h>
 
 
 LoRaModem modem;
-CayenneLPP lpp(51);
 bool connected = false;
 bool myWaitForDownlink = false;
 bool myDownLink = false;
@@ -26,7 +47,7 @@ const unsigned long myTimerDurationB = 5000;   // delay to wait for a downlink
 // Please enter your sensitive data in the Secret tab or arduino_secrets.h
 // Note: Best to have the App_Device hard coded. Run the program once to see the value.
 //#include "arduino_secrets.h"
-#define SECRET_APP_EUI "0000000000000000" 
+#define SECRET_APP_EUI "00000000000000000" 
 #define SECRET_APP_KEY "00000000000000000000000000000000"
 
 
@@ -147,56 +168,24 @@ void loop() {
 
 
     
-      lpp.reset();
-      float x = rand() / 10000000.0; //analogRead(A0)
-      lpp.addTemperature(1, x); 
+     
     
-      
-      /*
-      // Can do any of these
-      lpp.reset();
-      lpp.addDigitalInput(1, 0);
-      lpp.addDigitalOutput(2, 1);
-      lpp.addAnalogInput(3, 1.23f);
-      lpp.addAnalogOutput(4, 3.45f);
-      lpp.addLuminosity(5, 20304);
-      lpp.addPresence(6, 1);
-      lpp.addTemperature(7, 26.5f);
-      lpp.addRelativeHumidity(8, 86.6f);
-      lpp.addAccelerometer(9, 1.234f, -1.234f, 0.567f);
-      lpp.addBarometricPressure(10, 1023.4f);
-      lpp.addGyrometer(1, -12.34f, 45.56f, 89.01f);
-      lpp.addGPS(1, -12.34f, 45.56f, 9.01f);
-    
-      lpp.addUnixTime(1, 135005160);
-      
-      lpp.addGenericSensor(1, 4294967295);
-      lpp.addVoltage(1, 3.35);
-      lpp.addCurrent(1, 0.321);
-      lpp.addFrequency(1, 50);
-      lpp.addPercentage(1, 100);
-      lpp.addAltitude(1 , 50);
-      lpp.addPower(1 , 50000);
-      lpp.addDistance(1 , 10.555);
-      lpp.addEnergy(1 , 19.055);
-      lpp.addDirection(1 , 90);
-      lpp.addSwitch(1 , 0);
-      
-      lpp.addConcentration(1 , 512);
-      lpp.addColour(1 , 64, 128, 255);
-      
-      */
-    
-      
-      Serial.println();
-      Serial.println("Sending:" + String(x, 1));
-    
-    
-      int err;
-      modem.beginPacket();
-      modem.write(lpp.getBuffer(), lpp.getSize());
-      err = modem.endPacket(true);
-      if (err > 0) {
+  String msg = String(analogRead(A0));   // program expecting numbers!
+
+  Serial.println();
+  Serial.print("Sending: " + msg + " - ");
+  for (unsigned int i = 0; i < msg.length(); i++) {
+    Serial.print(msg[i] >> 4, HEX);
+    Serial.print(msg[i] & 0xF, HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+
+  int err;
+  modem.beginPacket();
+  modem.print(msg);
+  err = modem.endPacket(true);  
+     if (err > 0) {
         Serial.println("Message sent correctly!");
         digitalWrite(LEDR, HIGH); // new boards HIGH = off
         digitalWrite(LEDG, HIGH);
@@ -219,3 +208,5 @@ void loop() {
   }  // end timerA
 
 }
+
+
