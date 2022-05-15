0ae480b0bc333f1765ab1492bc1bef7eea5d27c7
Jeremy Ellis <keyfreemusic@gmail.com>
2022-05-15 15:32:32 -0700

    Update dot22a-blink-an-led.ino

--- examples/dot2-portenta-h7-with-accessories/dot22-actuators/dot22a-blink-an-led/dot22a-blink-an-led.ino
+++ examples/dot2-portenta-h7-with-accessories/dot22-actuators/dot22a-blink-an-led/dot22a-blink-an-led.ino
@@ -1,1 +1,21 @@
-dfg
+#include <Arduino.h> // Only needed by https://platformio.org/
+
+
+void setup() {
+   pinMode(D5, OUTPUT);  // just 5 if using the Seeeduino XIAO
+   Serial.begin(115200);
+}
+
+
+void loop() {
+   digitalWrite(D5, HIGH);      // sets LED on  
+   Serial.println("Led connected to D5 ON");     
+   delay(500);                  // waits 500ms (0.5 seconds)         
+ 
+   digitalWrite(D5, LOW);       // sets LED off         
+   Serial.println("D5 OFF");     
+   delay(2000);                 // waits 2000ms (2 seconds)        
+      
+}
+
+
