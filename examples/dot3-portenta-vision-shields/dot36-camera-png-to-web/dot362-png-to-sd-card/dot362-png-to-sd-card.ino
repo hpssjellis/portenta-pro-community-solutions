02e65ad2af5dbf3db84cb51841b54de4cdaa1f32
Jeremy Ellis <keyfreemusic@gmail.com>
2023-01-09 22:55:36 -0800

    Update dot362-png-to-sd-card.ino

--- examples/dot3-portenta-vision-shields/dot36-camera-png-to-web/dot362-png-to-sd-card/dot362-png-to-sd-card.ino
+++ examples/dot3-portenta-vision-shields/dot36-camera-png-to-web/dot362-png-to-sd-card/dot362-png-to-sd-card.ino
@@ -69,28 +69,32 @@ uint8_t ucOut[81920];    // works for 320x320
 // could use heap or Portenta SDRAM
 static uint8_t frame_buffer[CAM_WIDTH*CAM_HEIGHT] __attribute__((aligned(32)));
 
+char myFolder[10];
+// probably a really good idea to have myDealy greater than 3000 or you will fill up the SD card quickly
+int myDelay = 10000;  // default take a picture every 10 seconds
+int randomNumber = 0;
+
+
+
 void makePNG() {
   long l;
   l = micros();
 
-int myTime = millis()/1000; // seconds since the sketch began
-   char myPngFileName[] = "fs/myFolder6/my02----------.png";   // "fs/" needs to be there, think fileSystem
+  int myTime = millis()/1000; // seconds since the sketch began
+  
+   sprintf(myFolder, "fs/my%04d", randomNumber);  // Convert the random number to a string and store it in myFolder with "my" prepended 
+   mkdir(myFolder,0555);  // read and write access without run access for all levels 
+   
+   char myPngFileName[30];   // "fs/" needs to be there, think fileSystem
 
-  int myExtensionLength = 3;  // .txt = 3 .json = 4
-  // does anyone understand why the  + '0' is needed below???
-  myPngFileName[sizeof(myPngFileName)- myExtensionLength - 10] = myTime/10000000 % 10 + '0';
-  myPngFileName[sizeof(myPngFileName)- myExtensionLength - 9] = myTime/1000000 % 10 + '0';
-  myPngFileName[sizeof(myPngFileName)- myExtensionLength - 8] = myTime/100000 % 10 + '0';
-  myPngFileName[sizeof(myPngFileName)- myExtensionLength - 7] = myTime/10000 % 10 + '0';
-  myPngFileName[sizeof(myPngFileName)- myExtensionLength - 6] = myTime/1000 % 10 + '0';
-  myPngFileName[sizeof(myPngFileName)- myExtensionLength - 5] = myTime/100 % 10 + '0';
-  myPngFileName[sizeof(myPngFileName)- myExtensionLength - 4] = myTime/10 % 10 + '0';
-  myPngFileName[sizeof(myPngFileName)- myExtensionLength - 3] = myTime % 10 + '0';
+   // file name and path becomes myFolder + /myImg- + 000time + .png + null character
+   sprintf(myPngFileName, "%s/myImg-%08d.png\0", myFolder, myTime);  // Convert the random number to a string and store it in myFolder with "my" prepended
+
 
    myPngFile = fopen(myPngFileName, "w");          // "a" for append (add to file), "w" write, "r" read ?? 
-  int rc, iDataSize, x, y;
-  uint8_t ucLine[PNG_WIDTH];
-  rc = png.open(ucOut, sizeof(ucOut));
+   int rc, iDataSize, x, y;
+   uint8_t ucLine[PNG_WIDTH];
+   rc = png.open(ucOut, sizeof(ucOut));
 
   if (rc == PNG_SUCCESS) {
 
@@ -125,7 +129,9 @@ int myTime = millis()/1000; // seconds since the sketch began
 
       Serial.print("Printing info:");
       Serial.println(sizeof(ucOut));
-      Serial.println("  , file name: ");
+      Serial.print("Folder name: ");
+      Serial.println(myFolder);
+      Serial.print("File name with path: ");
       Serial.println(myPngFileName);
       
       l = micros() - l;
@@ -144,23 +150,26 @@ int myTime = millis()/1000; // seconds since the sketch began
 
 void setup() {
   Serial.begin(115200);      // initialize serial communication
-    pinMode(LED_BUILTIN, OUTPUT);      // set the LED pin mode
-    pinMode(LEDR, OUTPUT);
-    pinMode(LEDG, OUTPUT);
-    pinMode(LEDB, OUTPUT);
-    digitalWrite(LEDG, HIGH);
-    digitalWrite(LEDB, LOW);
-    digitalWrite(LEDR, HIGH);
-
-    while (!Serial && millis() < 10000); //either serial monitor or wait 10 seconds
-    digitalWrite(LEDB, HIGH);   
+  randomSeed(analogRead(0));  // Seed the random number generator
+  randomNumber = random(10000);  // Generate a random number between 0 and 9999
  
-  
+  pinMode(LED_BUILTIN, OUTPUT);      // set the LED pin mode
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+  digitalWrite(LEDG, HIGH);
+  digitalWrite(LEDB, LOW);
+  digitalWrite(LEDR, HIGH);
+
+  while (!Serial && millis() < 10000); //either serial monitor or wait 10 seconds
+  digitalWrite(LEDB, HIGH);   
+
+
 
   Serial.println("Mounting Camera...");
   // if (!cam.begin(CAMERA_R320x240, IMAGE_MODE, 30)) {
   //if (!cam.begin(CAMERA_R160x120, CAMERA_GRAYSCALE, 30)) {
-  if (!cam.begin(CAMERA_R320x320, CAMERA_GRAYSCALE, 30)) {
+  if (!cam.begin(CAMERA_R320x320, CAMERA_GRAYSCALE, 60)) {
     Serial.println("No Camera, make sure portenta Vision shield is connected");
   }
 
@@ -180,26 +189,24 @@ void setup() {
 
 void loop() {
  
-  // Make a folder if needed
-  mkdir("fs/myFolder6",0777);  // 0777 full access permissions linux style 
-   
+  
   Serial.println("------------------------- Taking a picture and making a PNG on the SD Card --------------------------------");
 
   // flash the blue LED to say a picture is going to be taken
   digitalWrite(LEDB, LOW); 
-  delay(500);
+  if (myDelay > 2000){delay(500);}
   digitalWrite(LEDB, HIGH);  
-  delay(500);
-  
+  if (myDelay > 2000){delay(500);}
+ 
   digitalWrite(LEDB, LOW); 
-  delay(1000);
+  if (myDelay > 2000){delay(500);}
   digitalWrite(LEDB, HIGH); 
-  delay(500);
+  if (myDelay > 2000){delay(500);}
   
   digitalWrite(LED_BUILTIN, LOW); 
   makePNG();
   digitalWrite(LED_BUILTIN, HIGH); 
   Serial.println("------------------------- Done and waiting 10 seconds --------------------------------");
-  delay(8000);   // wait a bit
+  delay(myDelay);   // wait a bit
   
 }
